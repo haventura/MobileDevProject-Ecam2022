32721e5ea38eba165764b27464e7d72a8aa53183
haventura <a.ventura@students.ephec.be>
2022-04-14 12:40:54 +0200

    added login structure

--- socialnetwork/lib/main.dart
+++ socialnetwork/lib/main.dart
@@ -2,40 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'dashboard_screen.dart';
+import 'login_screen.dart';
+import 'transition_route_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          SystemUiOverlayStyle.dark.systemNavigationBarColor,
+    ),
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  static const String _title = 'Sample App';
+ 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
+        textSelectionTheme:
+            const TextSelectionThemeData(cursorColor: Colors.orange),
+        // fontFamily: 'SourceSansPro',
+        textTheme: TextTheme(
+          headline3: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            // fontWeight: FontWeight.w400,
+            color: Colors.orange,
+          ),
+          button: const TextStyle(
+            // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+            fontFamily: 'OpenSans',
+          ),
+          caption: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.deepPurple[300],
+          ),
+          headline1: const TextStyle(fontFamily: 'Quicksand'),
+          headline2: const TextStyle(fontFamily: 'Quicksand'),
+          headline4: const TextStyle(fontFamily: 'Quicksand'),
+          headline5: const TextStyle(fontFamily: 'NotoSans'),
+          headline6: const TextStyle(fontFamily: 'NotoSans'),
+          subtitle1: const TextStyle(fontFamily: 'NotoSans'),
+          bodyText1: const TextStyle(fontFamily: 'NotoSans'),
+          bodyText2: const TextStyle(fontFamily: 'NotoSans'),
+          subtitle2: const TextStyle(fontFamily: 'NotoSans'),
+          overline: const TextStyle(fontFamily: 'NotoSans'),
+        ),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
+            .copyWith(secondary: Colors.orange),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      navigatorObservers: [TransitionRouteObserver()],
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        DashboardScreen.routeName: (context) => const DashboardScreen(),
+      },
     );
   }
 }
-
+/*
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -120,3 +160,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
